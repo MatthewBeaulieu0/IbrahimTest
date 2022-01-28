21c1eda9dc2d41afd0b1c487a376325ad2515ab3
IbrahimElyyan <ibrahimelyyan17@gmail.com>
2022-01-28 18:40:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ def sum(n):
     return total
 #I LOVE IBRAHIM
 print(sum(50))
+
+def sum1(n):
+    total1 = 0
+    for i in range(n + 69):
+        total1 += i
+    return total1
+#Ibrahims tingy
+print(sum1(50))
